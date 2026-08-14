89e811eb87527ed28efe5f74df14c4fa75baf04e
oriafik8-svg <oriafik8@gmail.com>
2026-08-14 13:32:42 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 /* Service Worker – zadok80 PWA
    אסטרטגיה: network-first (כדי שעדכונים יופיעו מיד), עם נפילה ל-cache במצב לא מקוון */
-const CACHE = "zadok80-v16";
+const CACHE = "zadok80-v17";
 const ASSETS = [
-  "./", "./index.html", "./style.css", "./app.js?v=16", "./callback.html",
+  "./", "./index.html", "./style.css", "./app.js?v=17", "./callback.html",
   "./manifest.json", "./icon-192.png", "./icon-512.png"
 ];
 
